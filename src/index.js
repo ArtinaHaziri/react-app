95daddb8e31f50a64f85b79c0f2f7d8fb34d756f
ArtinaHaziri <artinahaziri@gmail.com>
2024-11-20 20:00:28 +0100

    initalize repo

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { DestinationProvider } from './contex/DestinationContext';
+import { TravelProvider } from './contex/TravelContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <DestinationProvider >
+      <TravelProvider>
     <App />
+   </TravelProvider>
+
+   </DestinationProvider>
+  
+
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
